250146d0e7b475df4b70b38ab81c6496d3ce3832
Sunardi <71060025+naldy-dy@users.noreply.github.com>
2020-11-14 21:21:58 +0700

    move css login
    rapikan css login

--- system/resources/views/signup.blade.php
+++ system/resources/views/signup.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Login || Sign Up Form</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/style-login.css">
+	<link rel="stylesheet" type="text/css" href="{{url('public')}}/assets/css/style-login.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head> 
 <body>
